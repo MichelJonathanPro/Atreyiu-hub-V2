80e239d61cb761a541de794b92ca84b964ac4c54
Michel Jonathan <atreyiu@Michels-Mac-mini.local>
2026-03-21 08:08:11 +0100

    mise a jour des templates

--- Apps/Boutique/urls.py
+++ Apps/Boutique/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import BoutiqueIndexView
+from .views import BoutiqueIndexView, BoutiqueDetailView, BoutiquePanierView, BoutiquePaiementView
 
 app_name = 'boutique'
 
 urlpatterns = [
     path('', BoutiqueIndexView.as_view(), name='index'),
+    path('detail/', BoutiqueDetailView.as_view(), name='detail'),
+    path('panier/', BoutiquePanierView.as_view(), name='panier'),
+    path('paiement/', BoutiquePaiementView.as_view(), name='paiement'),
 ]
--- Apps/Boutique/views.py
+++ Apps/Boutique/views.py
@@ -2,3 +2,12 @@ from django.views.generic import TemplateView
 
 class BoutiqueIndexView(TemplateView):
     template_name = 'Boutique/Boutique_index.html'
+
+class BoutiqueDetailView(TemplateView):
+    template_name = 'Boutique/Boutique_detail.html'
+
+class BoutiquePanierView(TemplateView):
+    template_name = 'Boutique/Boutique_panier.html'
+
+class BoutiquePaiementView(TemplateView):
+    template_name = 'Boutique/Boutique_paiement.html'
